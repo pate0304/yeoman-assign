256ec1eac17821c91437391f2e984c3fd7812951
Jay Patel <pate0304@algonquinlive.com>
2016-10-15 20:17:57 -0400

    final

---
+++ scripts/controllers/jay.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myProjectApp.controller:JayCtrl
+ * @description
+ * # JayCtrl
+ * Controller of the myProjectApp
+ */
+angular.module('myProjectApp')
+  .controller('JayCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
